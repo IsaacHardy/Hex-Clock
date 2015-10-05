2ff3a49b875b2ab5a4dbcbe4def0b629a7d501fc
Isaac Hardy <isaachardyDEV@gmail.com>
2015-10-05 16:53:28 -0400

    Added formula that changed background-color based upon the time.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,5 @@
 (function (){
 
-
   // Time function
   function clock () {
 
@@ -11,21 +10,32 @@
     var minutes = today.getMinutes();
     var seconds = today.getSeconds();
 
+    console.log(hours);
+    console.log(minutes);
+    console.log(seconds);
+
     // Jquery Assignment to text ID
     var hourZero = (hours < 10) 
-      ? $('#hour').text('0' + hours) : $('#hour').text(hours);
+      ? $('#hour').text('#0' + hours) : $('#hour').text('#' + hours);
     var minZero = (minutes < 10) 
-      ? $('#minute').text(':0' + minutes) : $('#minute').text(':' + minutes);
+      ? $('#minute').text('0' + minutes) : $('#minute').text(minutes);
     var secZero = (seconds < 10) 
-      ? $('#second').text(':0' + seconds) : $('#second').text(':' + seconds);
+      ? $('#second').text('0' + seconds) : $('#second').text(seconds);
+
+    // Varable to combine time into one number
+    var total = hourZero.text() + minZero.text() + secZero.text();
+    console.log(total);
+
+    // Changes background color based upon var total
+    var totalNum = $('body').css('background-color', total);
+    console.log(totalNum);
 
   };
 
   // Call clock Function
   clock();
+
   // Run function clock every 1000 miliseconds
   setInterval(clock,1000);
-
-
 
 }());
